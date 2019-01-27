b3f3681d68a7ac617583eb8e65febbf18e592f62
Kunedo83 <45496737+Kunedo83@users.noreply.github.com>
2019-01-27 22:48:49 +0100

    Add files via upload

---
+++ Sourcecode/Control/ControlInterior.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+namespace SpaceBus
+{
+    public class ControlInterior : PartModule
+    {
+        private Animation anim = null;
+        private readonly string marion = "marijuana";
+        private readonly string marioff = "marijuana2";
+        public bool encendidaboton = false;
+        public override void OnStart(StartState state)
+        {
+            base.OnStart(state);
+            if (HighLogic.LoadedSceneIsFlight && FlightGlobals.ActiveVessel == part.vessel)
+            {
+                Init();
+            }
+        }
+        private void Init()
+        {
+            anim = part.internalModel.gameObject.GetChild("pot-neon").GetComponent<Animation>();                        
+        }
+        private void Update()
+        {
+            if (HighLogic.LoadedSceneIsFlight && FlightGlobals.ActiveVessel == part.vessel)
+            {
+                if (anim == null)
+                {
+                    Init();
+                }
+                if (anim != null)
+                {
+                    if (part.vessel.GetComponent<ModuleLight>().isOn != encendidaboton)
+                    {
+                        if (part.vessel.GetComponent<ModuleLight>().isOn)
+                        {
+                            anim.Play(marion);
+                        }
+                        if (!part.vessel.GetComponent<ModuleLight>().isOn)
+                        {
+                            anim.Play(marioff);
+                        }
+                        vessel.ActionGroups.SetGroup(KSPActionGroup.Light,part.vessel.GetComponent<ModuleLight>().isOn);
+                        encendidaboton = part.vessel.GetComponent<ModuleLight>().isOn;
+                    }                    
+                }
+            }
+        }        
+    }
+}
